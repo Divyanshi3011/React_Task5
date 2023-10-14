fa6c115eb5b052e2af37aff29441a7961339d489
Divyanshi <divyanshigupta3011@gmail.com>
2023-10-14 19:58:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthday: '1985-08-05',
+      showStats: false
+    };
+  }
+
+  timeSince(date) {
+    let today = new Date();
+    let otherDate = new Date(date);
+    let difference = Math.abs(today - otherDate);
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let years = Math.floor(days / 365);
+
+    return years;
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate, showStats: true });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Form>
+          <h1>Age Calculator</h1>
+          <h4>Enter your date of birth</h4>
+          <FormControl
+            className='control'
+            type='date'
+            onChange={event => this.setState({ newDate: event.target.value })}
+          />
+          {''}
+          <div>
+            <Button className='custom-button' onClick={() => this.changeBirthday()}>
+              Calculate Age
+            </Button>
+          </div>
+          {this.state.showStats ? (
+            <div className='fade age-stats'>
+              <h3>You are {this.timeSince(this.state.birthday)} years old</h3>
+            </div>
+          ) : (
+            <div />
+          )}
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default App;
